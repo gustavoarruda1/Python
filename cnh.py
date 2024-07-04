e2996fe2db180883b659f7b55efa2a7aaa404137
Gustavo Arruda <151965666+gustavoarruda1@users.noreply.github.com>
2024-07-03 22:04:26 -0300

    Add files via upload
    Verificando idade apta para dirigir

---
+++ cnh.py
@@ -0,0 +1,11 @@
+# --------Verificando idade para dirigir--------
+
+# Solicita ao usuário que digite sua idade
+idade = int(input("Digite sua idade:"))
+
+# Verifica se a idade é maior ou igual a 18
+if idade >= 18:
+    print("Você tem permissão para dirigir.")
+
+else:
+    print("Você não tem permissão para dirigir.")
